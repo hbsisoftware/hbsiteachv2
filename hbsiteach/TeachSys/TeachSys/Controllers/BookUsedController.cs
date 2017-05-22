996b614f34baf3fe32d9e19dc5b8efad30316e52
miaojiaxin <1144177146@qq.com>
2017-05-22 09:36:30 +0800

    教材使用

---
+++ hbsiteach/TeachSys/TeachSys/Controllers/BookUsedController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TeachSys.Controllers
+{
+    public class BookUsedController : Controller
+    {
+        //
+        // GET: /BookUsed/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
